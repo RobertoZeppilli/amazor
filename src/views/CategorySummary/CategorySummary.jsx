aea3489101e0bfbdede556a2ec4c9b4119c5207e
RobertoZeppilli <robzep171@gmail.com>
2022-03-12 15:36:44 +0100

    related categories from server

--- src/views/CategorySummary/CategorySummary.jsx
+++ src/views/CategorySummary/CategorySummary.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 // material components
 import { Grid } from '@material-ui/core'
@@ -9,13 +9,23 @@ import { CartCard } from '../../components'
 
 const CategorySummary = () => {
     const { category } = useParams()
-    console.log(category)
     const [featureProducts, setFeatureProducts] = useState([])
+    const [relatedCategories, setRelatedCategories] = useState([])
 
     const getFeturedProductsFromServer = async () => {
-        const dataFromServer = await fetch("http://localhost:5000/categories/2/featured")
-        const resFromServer = await dataFromServer.json()
-        setFeatureProducts(resFromServer)
+
+        try {
+            const res = await Promise.all([
+                fetch("http://localhost:5000/categories/2/featured"),
+                fetch("http://localhost:5000/categories/2/related"),
+            ]);
+            const data = await Promise.all(res.map(r => r.json()))
+            setFeatureProducts(data[0])
+            setRelatedCategories(data[1])
+        } catch (error) {
+            console.error(error)
+        }
+
     }
 
     useEffect(() => {
@@ -33,11 +43,20 @@ const CategorySummary = () => {
         </CartCard>
     </Grid>))
 
+    const relatedCategoriesJSX = relatedCategories.map((category, index) => (
+        <li key={`category-${index}`}>
+            <Link to={category.link}>{category.name}</Link>
+        </li>
+    ))
+
     return (
         <div>
             <Grid container spacing={3}>
                 {featureProductsJSX}
             </Grid>
+            <ul>
+                {relatedCategoriesJSX}
+            </ul>
         </div>
     )
 }
